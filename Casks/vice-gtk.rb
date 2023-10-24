6237bad00011f285c31bc636b8b38bc04c7c07f9
Janos LORINCZ <51958630+Loloke@users.noreply.github.com>
2023-10-24 21:14:21 +0200

    Update vice-gtk.rb

--- Casks/vice-gtk.rb
+++ Casks/vice-gtk.rb
@@ -5,7 +5,7 @@ cask "vice-gtk3" do
     sha256 arm:   "286fffa6307fa42e3e563589e88dc438b60e8913ed0431d2f5aa98c1bf9ee7a0",
            intel: "3a52f4d8403d1d8e7498efbce49f75aed392d0a1105c6a203462957366f09d4f"
 
-    url "https://sourceforge.net/projects/vice-emu/files/releases/binaries/macosx/vice-#{arch]-gtk3-3.7.1.dmg/download"
+    url "https://sourceforge.net/projects/vice-emu/files/releases/binaries/macosx/vice-#{arch}-gtk3-3.7.1.dmg/download"
     name "VICE-Emu"
     desc "The Versatile Commodore Emulator"
     homepage "https://vice-emu.sourceforge.io/"

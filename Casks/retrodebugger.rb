56661fea9b6dbdd5a00a65b02d338c3bd4df6b35
Janos LORINCZ <51958630+Loloke@users.noreply.github.com>
2024-12-07 00:07:19 +0100

    retrodebugger update to v64.68

--- Casks/retrodebugger.rb
+++ Casks/retrodebugger.rb
@@ -1,11 +1,11 @@
 cask "retrodebugger" do
-  version "v0.64.66"
-  sha256 "6a0a4920e589eefcb65118845f339b8cb39776ecabd6002c11aa0ef758cc6ab1"
+  version "v0.64.68"
+  sha256 "4332452fe83479663a36ea013cfa672c9c71aa9e846b43776a1fd839b9578692"
 
-  url "https://github.com/slajerek/RetroDebugger/releases/download/v0.64.66/RetroDebugger-v0.64.66-macOS.zip"
+  url "https://github.com/slajerek/RetroDebugger/releases/download/v0.64.68/RetroDebugger-v0.64.68-macOS.zip"
   name "Retro Debugger"
   desc "Debugger for 6502/6510 based retro systems, eg: C64, NES"
   homepage "https://github.com/slajerek/RetroDebugger"
 
-  app "Retro Debugger v0.64.66/Retro Debugger.app", target: "RetroDebugger.app"
+  app "Retro Debugger v0.64.68/Retro Debugger.app", target: "RetroDebugger.app"
 end
